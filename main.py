490a2f4caba3f8fbbf45509c0ea13e65dfc231a1
tuulosA <aleksi.tuulos@protonmail.com>
2024-11-29 19:24:10 +0200

    commit

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import logging
 import os
 from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler
-from datetime import datetime, timezone
 from reddit.commands import reddit_media_command
 
 # Load environment variables
 load_dotenv()
-
-start_time = datetime.now(timezone.utc)
-
 
 def setup_logging():
     logger = logging.getLogger()
@@ -26,26 +22,20 @@ def setup_logging():
 
     return logger
 
-
 def main():
-    # Set up logging
     logger = setup_logging()
     logger.info("Bot is starting...")
 
-    # Get the Telegram API key from environment variables
+    # Telegram API key from environment variables
     telegram_api_key = os.getenv('TELEGRAM_API_KEY')
     if not telegram_api_key:
         raise ValueError("TELEGRAM_API_KEY environment variable is not set. Please add it to your .env file.")
 
-    # Create the bot application
     application = Application.builder().token(telegram_api_key).build()
 
-    # Add the /r command handler
     application.add_handler(CommandHandler('r', reddit_media_command))
 
-    # Start the bot
     application.run_polling()
-
 
 if __name__ == '__main__':
     try:
